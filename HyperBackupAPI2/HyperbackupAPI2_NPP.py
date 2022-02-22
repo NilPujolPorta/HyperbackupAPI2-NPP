f92a548847839931d30c7b4d13751bc4b8769956
Nil Pujol <nilpujolporta@gmail.com>
2022-02-22 11:30:44 +0100

    Added json output and better errorhandling 0.1.2

--- HyperBackupAPI2/HyperbackupAPI2_NPP.py
+++ HyperBackupAPI2/HyperbackupAPI2_NPP.py
@@ -1,12 +1,11 @@
-from __future__ import barry_as_FLUFL
 import json
 import time
 from os.path import exists
 import os
+import datetime
 
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
-import selenium
 import argparse
 import mysql.connector
 import yaml
@@ -19,7 +18,7 @@ def main(args=None):
 	rutaJson = ruta+"/dadesHyperBackup2.json"
 	parser = argparse.ArgumentParser(description='')
 	parser.add_argument('-q', '--quiet', help='Nomes mostra els errors i el missatge de acabada per pantalla.', action="store_false")
-	parser.add_argument('--json-file', help='La ruta(fitxer inclos) a on es guardara el fitxer de dades json. Per defecte es:'+rutaJson, default=rutaJson, metavar='RUTA')
+	parser.add_argument('--json-file', help='La ruta(fitxer inclos) a on es guardara el fitxer de dades json. Per defecte es: '+rutaJson, default=rutaJson, metavar='RUTA')
 	parser.add_argument('-g', '--graphicUI', help='Mostra el navegador graficament.', action="store_false")
 	parser.add_argument('-v', '--versio', help='Mostra la versio', action='version', version='HyperBackupAPI-NPP v'+__version__)
 	args = parser.parse_args(args)
@@ -92,11 +91,15 @@ def main(args=None):
 
 	options = Options()
 	if args.graphicUI:
-		options.headless = True
-		options.add_argument('--headless')
-		options.add_argument('--disable-gpu')
+		#options.headless = True
+		#options.add_argument('--headless')
+		#options.add_argument('--disable-gpu')
 		options.add_argument('window-size=1720x980')
+		options.add_argument('log-level=1')#INFO = 0, WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3.
 	browser = webdriver.Chrome(executable_path= ruta+"/chromedriver.exe", options = options)
+
+	llistaNas = []
+	#per cada nas fer login i accedir al hyperbackup
 	for nas in resultatbd:
 		browser.get(nas[2])
 		time.sleep(10)
@@ -107,37 +110,77 @@ def main(args=None):
 		passwd = browser.find_element(by="xpath", value='//*[@id="dsm-pass-fieldset"]/div[1]/div/div[1]/input')
 		passwd.send_keys(nas[1])
 		browser.find_element(by="xpath", value='//*[@id="sds-login-vue-inst"]/div/span/div/div[2]/div[2]/div/div[3]/div[2]/div/div[2]/div[4]').click()
-		time.sleep(15)
+		time.sleep(20)
 		hypericon=browser.find_element(by="xpath", value='//*[@id="sds-desktop-shortcut"]/div/li[7]')
 		hypericon.click()
 		time.sleep(10)
+
+
+
+		#aconsegueix els noms de cada copia i els guarda en un array
 		nomsCopies = []
 		nomTots = browser.find_elements(by="class name", value="x-tree-node-anchor")
 		for nom in nomTots:
 			nomsCopies.append(nom.text)
+
+
+
+		#prep de variables
 		ultimaCorrecte = []
 		statusCopies = []
+
+
+
+		#aconsegueix els elements del menu de l'esquerra i els posa en un array
 		roottreenode = browser.find_elements(by="class name", value="x-tree-node")
+
+
+
+		#per cada element del menu de l'esquerra el clica i extreu les dades d'aquella copia
 		y=1
-		for treenode in roottreenode:								  #'/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div[     2    ]/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div/div/div
-			treenode.click()								      	  #'/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div[     1    ]/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div
-			time.sleep(2)									      	  #'/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div[     2    ]/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div
+		for treenode in roottreenode:
+			treenode.click()
+			time.sleep(2)
 			statusCopies.append(browser.find_element(by="xpath", value='/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div['+str(y)+']/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div').text)
-			if ((statusCopies[(y-1)]) != "Eliminando versiones de copia de seguridad...") and ((statusCopies[(y-1)]) !='Deleting backup versions...'):
+			if ((statusCopies[(y-1)]) != "Eliminando versiones de copia de seguridad...") and ((statusCopies[(y-1)]) !='Deleting backup versions...') and ((statusCopies[(y-1)]) !='Waiting...') and ((statusCopies[(y-1)]) !='Backing up...') and ((statusCopies[(y-1)]) !='Canceling...'):
 				ultimaCorrecte.append(browser.find_element(by="xpath", value='/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div['+str(y)+']/div/div/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/span').text)
-			                                                            	 #/html/body/div[11]/div[14]/div[3]/div[1]/div/div/div/div[2]/div[     1    ]/div/div/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/span
 			else:
 				ultimaCorrecte.append("Es sabra cuan acabi la copia actual")
 			y+=1
 		
+		#Mostra els resultats per pantalla 
 		
+		llistaCopies = []
 		x = 0
+		print()
+		print(nas[2])
+		print("=================================")
+		print()
 		while x < len(nomsCopies):
 			print(nomsCopies[x])
 			print("Status ultima copia: " + (statusCopies[x]))
 			print("Ultima copia correcte: " + (ultimaCorrecte[x]))
 			print()
+			llistaCopies.append({"Nom":nomsCopies[x], "Status ultima copia":statusCopies[x], "Ultima copia correcte":ultimaCorrecte[x]})
 			x+=1
+		llistaNas.append({"nomNAS":nas[2], "copies":llistaCopies})
+
+
+
+
+	#guardar resultats en un JSON
+	try:
+		with open(args.json_file, 'w') as f:
+			json.dump(llistaNas, f, indent = 4)
+	except Exception as e:
+		print("Error d'escriptura de json")
+		now = datetime.datetime.now()
+		date_string = now.strftime('%Y-%m-%d--%H-%M-%S-json')
+		f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+		f.write("Error d'escriptura de json "+str(e))
+		f.close()
+	if not(args.quiet):
+		print("Done")
 
 if __name__ =='__main__':
     main()
